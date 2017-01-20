530365996bafa1b965b08a2fb48c846a805e63b7
Alec <ajpershick@gmail.com>
2017-01-19 23:14:38 -0800

    Initial website upload
    Just moving work in progress website to Git.

---
+++ StopFlood/contact-form-handler.php
@@ -0,0 +1,68 @@
+<?php 
+error_reporting(-1);
+ini_set('display_errors', 'On');
+set_error_handler("var_dump");
+$errors = '';
+$myemail = 'info@stopflood.ca';//<-----Put Your email address here.
+if(empty($_POST['name'])  || 
+   empty($_POST['email']) || 
+   empty($_POST['message']))
+{
+    $errors .= "\n Error: all fields are required";
+}
+
+$name = $_POST['name']; 
+$email_address = $_POST['email']; 
+$message = $_POST['message']; 
+
+if (!preg_match(
+"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
+$email_address))
+{
+    $errors .= "\n Error: Invalid email address";
+}
+
+if( empty($errors))
+{
+	$to = $myemail; 
+	$email_subject = "Contact form submission: $name";
+	$email_body = "You have received a new message. ".
+	" Here are the details:\n \n Name: $name \n Email: $email_address \n \n Message: $message"; 
+	
+	$headers = "From: $myemail\n"; 
+	$headers .= "Reply-To: $email_address";
+	
+	mail($to,$email_subject,$email_body,$headers);
+	//redirect to the 'thank you' page
+	header('Location: contact-form-thank-you.html');
+} 
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
+<html>
+<head>
+<style>
+h1
+{
+	font-family : Arial, Helvetica, sans-serif;
+	font-size : 50px;
+    font-weight : bold;
+}
+label,a,body
+{
+	font-family : Arial, Helvetica, sans-serif;
+	font-size : 30px; 
+}
+
+</style>	
+	<title>Contact form handler</title>
+</head>
+
+<body>
+<!-- This page is displayed only if there is some error -->
+<?php
+echo nl2br($errors);
+?>
+
+
+</body>
+</html>
